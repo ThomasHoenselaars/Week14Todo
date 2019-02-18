1806aa01261f7a90225b25bc63502683f418ba9c
Thomas Hoenselaars <heemsteda@gmail.com>
2019-02-18 11:07:31 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// variabelen pakken
+const addButton = document.querySelector('.add-button');
+const todoInput = document.querySelector('.todo-input');
+
+const todoList = document.querySelector('.todo-list');
+
+
+// addItem
+// todoInput value - on click - inner html todoList - daarna weer input leegmaken
+const addItem = () => {
+    if (todoInput.value === "") {
+        alert('A to do cant be nothing');
+        return;
+    }
+    let todoItem = todoInput.value;
+
+    todoList.innerHTML += `<li class="list-item">${todoItem}<button type="button" class=remove-button>X</button></li>`;
+    
+    todoInput.value = ""; 
+    
+};
+
+// removeItem - zoek event target - verwijder parent van event target
+const removeItem = (e) => {
+    if(e.target && e.target.nodeName === "BUTTON") {
+    e.target.parentNode.remove();
+    }
+};
+
+// allChecked - als alle li's in de ul class complete hebben, alert klaar!
+
+const allChecked = () => {
+    if (todoList.children.length === 0) {
+        console.log("There are no children");
+        return;
+    } else if (todoList.children.length === todoList.querySelectorAll(".complete").length) {
+        alert("You have completed all your to do's");
+    };
+};
+
+// markChecked - negeren als er op de removebutton geklikt wordt, anders complete class toevoegen
+const markChecked = (e) => {
+    if (e.target.nodeName === "BUTTON") {
+        return;
+    } else {
+        e.target.classList.toggle('complete');
+    }
+    allChecked();
+}
+
+
+// clickevents
+addButton.addEventListener('click', addItem);
+
+todoList.addEventListener('click', removeItem);
+todoList.addEventListener('click', markChecked);
+
+
